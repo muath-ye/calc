7718492834af5659b4a5a4c205f4ef0e4ccf0531
Muathye <muath.ye@gmail.com>
2021-02-24 06:30:43 +0300

    add helpers.php

---
+++ src/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+use MuathYe\Utilities\Calculator;
+
+/**
+ * Returns the Calculator instance
+ *
+ * @return Calculator
+ */
+function calc() : Calculator
+{
+    return new Calculator;
+}
