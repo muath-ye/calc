67a61fe43c3756f0ac5e0b30925c4e6588d3ebee
Muathye <muath.ye@gmail.com>
2021-02-24 05:47:17 +0300

    Add service provider

---
+++ src/Providers/CalculatorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MuathYe\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
